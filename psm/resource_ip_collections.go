f9223fd8628e133f4ceb8b51138b4a58d31a102f
Farsonic <fp@prowsehouse.com>
2023-10-25 14:11:37 +1000

    removed debug code and bumped version

--- psm/resource_ip_collections.go
+++ psm/resource_ip_collections.go
@@ -5,7 +5,6 @@ import (
 	"context"
 	"encoding/json"
 	"io"
-	"log"
 	"net/http"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
@@ -65,9 +64,6 @@ func resourceIPCollectionCreate(ctx context.Context, d *schema.ResourceData, m i
 		return diag.FromErr(err)
 	}
 
-	// Log the JSON being sent in the request
-	log.Printf("[DEBUG] Sending IPCollection JSON to the server: %s", string(jsonBytes))
-
 	req, err := http.NewRequestWithContext(ctx, "POST", config.Server+"/configs/network/v1/tenant/default/ipcollections", bytes.NewBuffer(jsonBytes))
 	if err != nil {
 		return diag.FromErr(err)
@@ -75,22 +71,13 @@ func resourceIPCollectionCreate(ctx context.Context, d *schema.ResourceData, m i
 
 	req.AddCookie(&http.Cookie{Name: "sid", Value: config.SID})
 
-	// Log the full request details
-	log.Printf("[DEBUG] Request details: Method: %s, URL: %s, Headers: %v", req.Method, req.URL, req.Header)
-
 	resp, err := client.Do(req)
 	if err != nil {
 		return diag.FromErr(err)
 	}
 	defer resp.Body.Close()
 
-	// Log the response status code and headers
-	log.Printf("[DEBUG] Response status: %s, Headers: %v", resp.Status, resp.Header)
-
 	bodyBytes, _ := io.ReadAll(resp.Body)
-	// Log the response body
-	log.Printf("[DEBUG] Response body: %s", string(bodyBytes))
-
 	if resp.StatusCode != http.StatusOK {
 		return diag.Errorf("failed to create ip_collection: HTTP %d %s: %s", resp.StatusCode, resp.Status, bodyBytes)
 	}
