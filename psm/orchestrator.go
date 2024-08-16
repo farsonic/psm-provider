ec8350e38e210a8a9864163fd5d796f83aefe67c
Max Power <10763458+Max1211@users.noreply.github.com>
2024-08-16 20:02:53 +0200

    Error correction

--- psm/orchestrator.go
+++ psm/orchestrator.go
@@ -19,6 +19,9 @@ func resourceOrchestrator() *schema.Resource {
 		ReadContext:   resourceOrchestratorRead,
 		UpdateContext: resourceOrchestratorUpdate,
 		DeleteContext: resourceOrchestratorDelete,
+		Importer: &schema.ResourceImporter{
+			StateContext: resourceOrchestratorImport,
+		},
 		Schema: map[string]*schema.Schema{
 			"type": {
 				Type:     schema.TypeString,
@@ -54,12 +57,13 @@ func resourceOrchestrator() *schema.Resource {
 			"disable_server_authentication": {
 				Type:     schema.TypeBool,
 				Optional: true,
-				Default:  true,
+				Computed: true,
 				ForceNew: false,
 			},
 			"namespaces": {
 				Type:     schema.TypeList,
 				Optional: true,
+				Computed: true,
 				Elem: &schema.Resource{
 					Schema: map[string]*schema.Schema{
 						"name": {
@@ -120,15 +124,16 @@ func resourceOrchestratorCreate(ctx context.Context, d *schema.ResourceData, m i
 	orchestrator.Spec.Credentials.Password = d.Get("password").(string)
 
 	caData, hasCaData := d.GetOk("ca_data")
-	disableServerAuth, hasDisableServerAuth := d.GetOk("disable_server_authentication")
-
 	if hasCaData {
 		orchestrator.Spec.Credentials.CAData = caData.(string)
 		orchestrator.Spec.Credentials.DisableServerAuthentication = false
-	} else if hasDisableServerAuth {
-		orchestrator.Spec.Credentials.DisableServerAuthentication = disableServerAuth.(bool)
 	} else {
-		orchestrator.Spec.Credentials.DisableServerAuthentication = true
+		disableServerAuth, hasDisableServerAuth := d.GetOk("disable_server_authentication")
+		if hasDisableServerAuth {
+			orchestrator.Spec.Credentials.DisableServerAuthentication = disableServerAuth.(bool)
+		} else {
+			orchestrator.Spec.Credentials.DisableServerAuthentication = true
+		}
 	}
 
 	if v, ok := d.GetOk("namespaces"); ok {
@@ -142,7 +147,7 @@ func resourceOrchestratorCreate(ctx context.Context, d *schema.ResourceData, m i
 			}
 		}
 	} else {
-		// Set default namespace if not specified
+		// Always include the default namespace in the API request
 		orchestrator.Spec.Namespaces = []Namespace{
 			{
 				Name: "all_namespaces",
@@ -227,16 +232,32 @@ func resourceOrchestratorRead(ctx context.Context, d *schema.ResourceData, m int
 	d.Set("type", orchestrator.Spec.Type)
 	d.Set("uri", orchestrator.Spec.URI)
 	d.Set("username", orchestrator.Spec.Credentials.Username)
-	d.Set("ca_data", orchestrator.Spec.Credentials.CAData)
 
-	namespaces := make([]map[string]interface{}, len(orchestrator.Spec.Namespaces))
-	for i, ns := range orchestrator.Spec.Namespaces {
-		namespaces[i] = map[string]interface{}{
-			"name": ns.Name,
-			"mode": ns.Mode,
-		}
+	if orchestrator.Spec.Credentials.CAData != "" {
+		d.Set("ca_data", orchestrator.Spec.Credentials.CAData)
+		d.Set("disable_server_authentication", false)
+	} else {
+		d.Set("disable_server_authentication", orchestrator.Spec.Credentials.DisableServerAuthentication)
 	}
-	d.Set("namespaces", namespaces)
+
+	// Handle namespaces
+	if len(orchestrator.Spec.Namespaces) == 0 {
+		d.Set("namespaces", []interface{}{
+			map[string]interface{}{
+				"name": "all_namespaces",
+				"mode": "smartservicemonitored",
+			},
+		})
+	} else {
+		namespaces := make([]interface{}, len(orchestrator.Spec.Namespaces))
+		for i, ns := range orchestrator.Spec.Namespaces {
+			namespaces[i] = map[string]interface{}{
+				"name": ns.Name,
+				"mode": ns.Mode,
+			}
+		}
+		d.Set("namespaces", namespaces)
+	}
 
 	return nil
 }
@@ -256,15 +277,16 @@ func resourceOrchestratorUpdate(ctx context.Context, d *schema.ResourceData, m i
 	orchestratorCurrent.Spec.Credentials.Password = d.Get("password").(string)
 
 	caData, hasCaData := d.GetOk("ca_data")
-	disableServerAuth, hasDisableServerAuth := d.GetOk("disable_server_authentication")
-
 	if hasCaData {
 		orchestratorCurrent.Spec.Credentials.CAData = caData.(string)
 		orchestratorCurrent.Spec.Credentials.DisableServerAuthentication = false
-	} else if hasDisableServerAuth {
-		orchestratorCurrent.Spec.Credentials.DisableServerAuthentication = disableServerAuth.(bool)
 	} else {
-		orchestratorCurrent.Spec.Credentials.DisableServerAuthentication = true
+		disableServerAuth, hasDisableServerAuth := d.GetOk("disable_server_authentication")
+		if hasDisableServerAuth {
+			orchestratorCurrent.Spec.Credentials.DisableServerAuthentication = disableServerAuth.(bool)
+		} else {
+			orchestratorCurrent.Spec.Credentials.DisableServerAuthentication = true
+		}
 	}
 
 	if v, ok := d.GetOk("namespaces"); ok {
@@ -356,4 +378,71 @@ func resourceOrchestratorDelete(ctx context.Context, d *schema.ResourceData, m i
 	d.SetId("")
 
 	return nil
+}
+
+func resourceOrchestratorImport(ctx context.Context, d *schema.ResourceData, m interface{}) ([]*schema.ResourceData, error) {
+	config := m.(*Config)
+	client := config.Client()
+
+	// The ID is expected to be the name of the orchestrator
+	name := d.Id()
+
+	url := fmt.Sprintf("%s/configs/orchestration/v1/orchestrator/%s", config.Server, name)
+
+	req, err := http.NewRequestWithContext(ctx, "GET", url, nil)
+	if err != nil {
+		return nil, fmt.Errorf("error creating request: %s", err)
+	}
+
+	req.AddCookie(&http.Cookie{Name: "sid", Value: config.SID})
+
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, fmt.Errorf("error making request: %s", err)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("failed to import Orchestrator: HTTP %s", resp.Status)
+	}
+
+	orchestrator := &Orchestrator{}
+	if err := json.NewDecoder(resp.Body).Decode(orchestrator); err != nil {
+		return nil, fmt.Errorf("error decoding response: %s", err)
+	}
+
+	d.SetId(orchestrator.Meta.UUID)
+	d.Set("name", orchestrator.Meta.Name)
+	d.Set("type", orchestrator.Spec.Type)
+	d.Set("uri", orchestrator.Spec.URI)
+	d.Set("username", orchestrator.Spec.Credentials.Username)
+	d.Set("disable_server_authentication", orchestrator.Spec.Credentials.DisableServerAuthentication)
+
+	// Don't set the password, as it's not returned by the API
+	// The user will need to set this manually after import
+
+	if orchestrator.Spec.Credentials.CAData != "" {
+		d.Set("ca_data", orchestrator.Spec.Credentials.CAData)
+	}
+
+	if len(orchestrator.Spec.Namespaces) > 0 {
+		namespaces := make([]map[string]interface{}, len(orchestrator.Spec.Namespaces))
+		for i, ns := range orchestrator.Spec.Namespaces {
+			namespaces[i] = map[string]interface{}{
+				"name": ns.Name,
+				"mode": ns.Mode,
+			}
+		}
+		d.Set("namespaces", namespaces)
+	} else {
+		// Set default namespace if none are specified
+		d.Set("namespaces", []map[string]interface{}{
+			{
+				"name": "all_namespaces",
+				"mode": "smartservicemonitored",
+			},
+		})
+	}
+
+	return []*schema.ResourceData{d}, nil
 }
